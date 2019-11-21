f7032470f2275415f1a325baf38be2c10134a013
MaximEmelyanchenko <beta3-z@mail.ru>
2019-11-22 00:08:29 +0300

    Remove Shadow namespace

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import {Utils} from './SiebelApp'
 
-declare function theApplication(): Shadow.Application
+declare function theApplication(): ShadowApplication
 declare function IsOpenUI(): boolean
 declare function IsOfflineModeEnabled(): boolean
 declare function SWEGotoView(viewName: string): void
@@ -9,36 +9,34 @@ declare function Namespace(name: string): void
 
 declare var utils: Utils
 
-declare namespace Shadow {
-    interface Application {
-        ActiveApplet(): Shadow.Applet
-        ActiveBusComp(): any
-        ActiveBusObject(): any
-        ActiveViewName(): any
-        FindApplet(name): any
-        FindBusObject(name): any
-        GetProfileAttr(name): any
-        GetSRN(): any
-        GetService(name): any
-        InvokeMethod(name, inputPropSet): any
-        IsReady(): any
-        Name(): any
-        NewPropertySet(): any
-        SWEAlert(text): any
-        SeblTrace(category, trcMessage): any
-        SetProfileAttr(name, value): any
-        ShowModalDialog(url, argin, options): any
-        TriggerUPTEvent(inputPropSet): any
-    }
+interface ShadowApplication {
+    ActiveApplet(): ShadowApplet
+    ActiveBusComp(): any
+    ActiveBusObject(): any
+    ActiveViewName(): any
+    FindApplet(name): any
+    FindBusObject(name): any
+    GetProfileAttr(name): any
+    GetSRN(): any
+    GetService(name): any
+    InvokeMethod(name, inputPropSet): any
+    IsReady(): any
+    Name(): any
+    NewPropertySet(): any
+    SWEAlert(text): any
+    SeblTrace(category, trcMessage): any
+    SetProfileAttr(name, value): any
+    ShowModalDialog(url, argin, options): any
+    TriggerUPTEvent(inputPropSet): any
+}
 
-    interface Applet {
-        ActiveMode(): any
-        BusComp(): any
-        BusObject(): any
-        FindActiveXControl(controlName): any
-        FindControl(controlName): any
-        InvokeMethod(name, inputPropSet): any
-        Name(): any
-        ReInit(): any
-    }
+interface ShadowApplet {
+    ActiveMode(): any
+    BusComp(): any
+    BusObject(): any
+    FindActiveXControl(controlName): any
+    FindControl(controlName): any
+    InvokeMethod(name, inputPropSet): any
+    Name(): any
+    ReInit(): any
 }
