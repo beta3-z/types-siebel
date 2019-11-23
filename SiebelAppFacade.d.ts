8cac941e154370256d822821ee9f08eaa042cebe
MaximEmelyanchenko <beta3-z@mail.ru>
2019-11-23 18:08:15 +0300

    Description for few PresentationModel methods

--- SiebelAppFacade.d.ts
+++ SiebelAppFacade.d.ts
@@ -12,10 +12,39 @@ export var ComponentMgr: {
 }
 
 export class BasePM {
-    AddMethod(t,n,r): void
-    AddProperty(t,n,r): any
-    AddValidator(e,t): any
-    AttachEventHandler(e,t,n): any
+    /**
+     * Adds a method to a presentation model. You can use ExecuteMethod to run the method that
+     * AddMethod adds from the presentation model or from the physical renderer.
+     *
+     * If AddMethod attempts to add a new method that the predefined client already contains, then the new method
+     * becomes a customization of the predefined method, and this customization runs before or after the predefined
+     * method depending on the CancelOperation part of the return value.
+     *
+     * A method that customizes another method can return to the caller without running the method that it customizes. To do
+     * this, you configure Siebel Open UI to set the CancelOperation part of the return value to true. You set this property on
+     * the ReturnStructure object that Siebel Open UI sends to each method as an argument.
+     *
+     * @param name is a string that contains the name of the method that Siebel Open UI adds to the presentation model
+     * @param methodDef is an argument that allows you to call a method or a method reference
+     * @param config
+     *
+     */
+    AddMethod(name: string, methodDef: (...args: any[]) => void, config?: MethodConfig): boolean
+
+    /**
+     * Adds a property to a presentation model. Siebel Open UI can access it through the Get method.
+     */
+    AddProperty(name: string, value: any): boolean
+
+    /**
+     * Validates an event. It allows you to write a custom validation for any event.
+     */
+    AddValidator(eventName: string, validationFunction: (...args: any[]) => boolean): boolean
+
+    /**
+     * Attaches an event handler to an even
+     */
+    AttachEventHandler(eventName: string, handler: (...args: any[]) => boolean): boolean
     AttachNotificationHandler(t,n,r): any
     AttachPMBinding(t,n,r): any
     AttachPSHandler(t,n,r): any
@@ -24,7 +53,7 @@ export class BasePM {
     DecorateEventHandlers(): any
     EndLife(): any
     ExecuteMethod(e): any
-    Get(e,t): any
+    Get(name: string): any
     HandleNotify(e): any
     HandlePostExecute(e,t,n): any
     HandlePreExecute(e,t,n): any
@@ -34,6 +63,17 @@ export class BasePM {
     SetProperty(t,n,r): any
     Setup(n): any
     Show(): any
+}
+
+/**
+ * @property sequence - if true, then Siebel Open UI calls methodName before it calls the method that already exists in the presentation model.
+ * @property override - if true, then Siebel Open UI does not call the method that already exists in the presentation model
+ * @property scope - describes the scope
+ */
+interface MethodConfig {
+    sequence: boolean
+    override: boolean
+    scope: any
 }
 
 export class PresentationModel extends BasePM {
--- SiebelJS.d.ts
+++ SiebelJS.d.ts
@@ -2,4 +2,4 @@ export as namespace SiebelJS
 
 export function Namespace(name: string): void
 export function Dependency(name: string): any
-export function Extend<T, S>(target: T, superclass: S): T & S
+export function Extend<T, S>(target: T, superclass: S): void
